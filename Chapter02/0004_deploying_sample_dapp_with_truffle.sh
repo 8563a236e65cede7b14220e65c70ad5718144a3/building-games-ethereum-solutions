5c24c91f4afe26753006379c907273653f596567
Warren Jitsing <warren.jitsing@gmail.com>
2020-08-28 10:52:24 +0200

    Finish Chapter 2
    See: Building Games with Ethereum Smart Contracts Chaper 2
    
    Why: Learn about truffle, geth
    
    How: truffle, geth
    
    Tags: devlopment environment

---
+++ Chapter02/0004_deploying_sample_dapp_with_truffle.sh
@@ -0,0 +1,13 @@
+# Create truffle test directory
+mkdir truffle-test
+cd truffle-test
+
+# Init for truffle development
+truffle init
+
+# Start command console
+truffle develop
+
+# Run in Truffle dev console
+## migrate
+## .exit
